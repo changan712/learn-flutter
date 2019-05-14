b112954590272cde5f6398238867649b74af4197
viola <9978707@qq.com>
2019-05-15 07:02:14 +0800

    counter #2

--- lib/counter/home.dart
+++ lib/counter/home.dart
@@ -6,17 +6,43 @@ class HomePage extends StatefulWidget {
 
   @override
   State<StatefulWidget> createState() {
-    // TODO: implement createState
-    return null;
+    return new _MyHomePageState();
   }
 }
 
-class _myHomePageState extends State<HomePage>{
+class _MyHomePageState extends State<HomePage> {
+  int _counter = 0;
+
   @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    return null;
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
   }
 
-
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text(widget.title),
+      ),
+      body: new Center(
+        child: new Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            new Text('you have pushed the buttong many times'),
+            new Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.display1,
+            )
+          ],
+        ),
+      ),
+      floatingActionButton: new FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: new Icon(Icons.add),
+      ),
+    );
+  }
 }
